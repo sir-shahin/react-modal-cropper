49cf43d15907efbd0310c7498aa25a5bda6e218e
shahin norouzi <shahin.n14001995@gmail.com>
2021-05-09 11:42:11 +0430

    show compenent in a react project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Cropper from './components/Cropper';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Cropper cropperId='img1'></Cropper>
+      </div>
+    </div>
+  );
+}
+
+export default App;
